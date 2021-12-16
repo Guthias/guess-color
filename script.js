92feefbdc6c3568941563bf96715985a634b9490
guthias <gmgustavom@gmail.com>
2021-12-16 17:35:30 -0300

    Adicionando cor na linha

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
+let filledRow = 1;
+
 function eventToColors() {
   const colors = document.querySelectorAll('.select-color');
 
   colors.forEach((element) => {
     element.addEventListener('click' , () => {
       let elementClass = element.className;
-      elementClass = elementClass.replace('select-color', '');
-      console.log(elementClass);
+      elementClass = elementClass.replace('select-color ', '');
+      clickColor(elementClass);
     });
   })
+}
+
+function clickColor(colorName) {
+  const rowList = document.querySelectorAll('.row');
+  const rowColors = rowList[filledRow].querySelectorAll('.colors');
+  Array.from(rowColors).find(element => element.classList.length === 1).classList.add(colorName);
 }
 
 window.onload = () => {
